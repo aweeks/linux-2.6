070fa88a6f938e9e75e703ebd6cdb75e56bfa319
Alex Weeks <alex.weeks@gmail.com>
2010-12-02 19:29:12 -0800

    Added length argument

--- loghilight.sh
+++ loghilight.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-for line in `tail -n 10 /var/log/syslog`
+for line in `tail -n ${1} /var/log/syslog`
 do
     echo ${line} | GREP_COLOR='1;32' grep ADD --color=ALWAYS
     echo ${line} | GREP_COLOR='1;31' grep DSP --color=ALWAYS
